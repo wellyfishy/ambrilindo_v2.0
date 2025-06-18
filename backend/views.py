b89a1acfb31fa009a796f29914358a67caafbf32
wellyfishy <wellyfishy@gmail.com>
2025-06-18 16:13:30 +0800

    nothing

--- backend/views.py
+++ backend/views.py
@@ -116,7 +116,15 @@ def admin_dashboard(request, event_pk):
     nomor_tandings = NomorTanding.objects.filter(event=event)
     
     bagans = Bagan.objects.filter(event=event)
-
+    # count = 0
+    # for bagan in bagans:
+    #     dbs = DetailBagan.objects.filter(bagan=bagan)
+    #     for db in dbs:
+    #         if db.atlet1:
+    #             count += 1
+    #         if db.atlet2:
+    #             count += 1
+    # print(count)
     custom_order = [1, 8, 4, 5, 2, 7, 3, 6]
 
     atlet_assignment1 = {
